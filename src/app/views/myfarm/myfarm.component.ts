c043a2388a656ed08c2184196471929ff688aa74
Faruk Terzioğlu <faruk.terzioglu@hotmail.com>
2018-07-01 14:52:40 +0300

    progress

--- src/app/views/myfarm/myfarm.component.ts
+++ src/app/views/myfarm/myfarm.component.ts
@@ -10,13 +10,14 @@ export class MyfarmComponent implements OnInit {
     scaleShowVerticalLines: false,
     responsive: true
   };
-  public barChartLabels: string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartLabels: string[] = ['2012', '2013', '2014', '2015', '2016', '2017', '2018'];
   public barChartType = 'bar';
   public barChartLegend = true;
 
   public barChartData: any[] = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Hatçe Ananın Çiftliği'},
+    {data: [35, 32, 75, 25, 10, 65, 95], label: 'Karadeniz Uşağı'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Northern Tiger'}
   ];
 
   // Pie
