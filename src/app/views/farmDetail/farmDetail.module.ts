0cb2568204735523a0d1a42260774f0c23ef2dc3
Faruk Terzioğlu <faruk.terzioglu@hotmail.com>
2018-07-01 13:22:15 +0300

    progress

--- src/app/views/farmDetail/farmDetail.module.ts
+++ src/app/views/farmDetail/farmDetail.module.ts
@@ -8,8 +8,60 @@ import { ButtonsModule } from 'ngx-bootstrap/buttons';
 import { FarmDetailComponent } from './farmDetail.component';
 import { FarmDetailRoutingModule } from './farmDetail-routing.module';
 
+import { Component } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
+
+@Component({
+  selector: 'mapp',
+  styles: [`
+    agm-map {
+      height: 300px;
+    }
+  `],
+  template: `
+  <agm-map [latitude]="lat" [longitude]="lng" [zoom]="zoom" [mapTypeId]="'hybrid'">
+    <agm-marker 
+      *ngFor="let m of markers; let i = index"
+      [latitude]="m.lat"
+      [longitude]="m.lng"
+      [label]="m.label">
+      
+    <agm-info-window>
+      <strong></strong>
+    </agm-info-window>
+    </agm-marker>
+  </agm-map>
+  `
+})
+export class MAppComponent {
+  mapType : "roadmap";
+  zoom: number = 16;
+  lat: number = 37.843646;
+  lng: number = 32.570062;
+
+  markers: marker[] = [
+	  {
+		  lat: 37.843646,
+		  lng: 32.570062,
+		  label: '',
+		  draggable: false
+	  }
+  ]
+}
+
+// just an interface for type safety.
+interface marker {
+	lat: number;
+	lng: number;
+	label?: string;
+	draggable: boolean;
+}
+
 @NgModule({
   imports: [
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAgWQllVG7xPTr53uWFmiBkZ12tWJevY5o'
+    }),
     CommonModule,
     FormsModule,
     FarmDetailRoutingModule,
@@ -17,6 +69,6 @@ import { FarmDetailRoutingModule } from './farmDetail-routing.module';
     BsDropdownModule,
     ButtonsModule.forRoot()
   ],
-  declarations: [ FarmDetailComponent ]
+  declarations: [ FarmDetailComponent, MAppComponent ]
 })
 export class FarmDetailModule { }
